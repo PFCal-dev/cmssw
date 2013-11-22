1dba357f0ba8a1345a27f1ab75bfcc5b281ad6f3
Pedro Silva <psilva@cern.ch>
2013-11-22 15:33:37 +0100

    Adding geometry records for pfcal

---
+++ Geometry/Records/interface/PFCalEERecoGeometryRcd.h
@@ -0,0 +1,6 @@
+#ifndef Geometry_PFCalEERecoGeometryRcd_H
+#define Geometry_PFCalEERecoGeometryRcd_H
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class PFCalEERecoGeometryRcd : public edm::eventsetup::EventSetupRecordImplementation<PFCalEERecoGeometryRcd> {};
+#endif
