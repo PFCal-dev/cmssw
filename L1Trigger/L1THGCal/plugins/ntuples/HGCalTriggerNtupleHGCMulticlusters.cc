6009268d1e195170ea86d8e6b2fec4975277a9fb
bcourbon <benoit.courbon@cern.ch>
2017-07-24 14:33:33 +0200

    clean version

--- L1Trigger/L1THGCal/plugins/ntuples/HGCalTriggerNtupleHGCMulticlusters.cc
+++ L1Trigger/L1THGCal/plugins/ntuples/HGCalTriggerNtupleHGCMulticlusters.cc
@@ -17,7 +17,6 @@ class HGCalTriggerNtupleHGCMulticlusters : public HGCalTriggerNtupleBase
   private:
     virtual void clear() override final;
 
-
     edm::EDGetToken multiclusters_token_;
 
     int cl3d_n_ ;
