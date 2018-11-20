07e9f5c3ea18658f52971929c625837f22bd62f4
Samuel Webb <samuel.webb@cern.ch>
2018-11-20 16:35:18 +0100

    Add new modified maximum algorithm

--- L1Trigger/L1THGCal/interface/backend/HGCalMulticlusteringHistoImpl.h
+++ L1Trigger/L1THGCal/interface/backend/HGCalMulticlusteringHistoImpl.h
@@ -58,6 +58,8 @@ private:
 
     std::vector<GlobalPoint> computeInterpolatedMaxSeeds( const Histogram & histoClusters );
 
+    std::vector<GlobalPoint> computeModifiedMaxSeeds( const Histogram & histoClusters );
+
     std::vector<GlobalPoint> computeThresholdSeeds( const Histogram & histoClusters );
 
     std::vector<l1t::HGCalMulticluster> clusterSeedMulticluster(const std::vector<edm::Ptr<l1t::HGCalCluster>> & clustersPtrs,
