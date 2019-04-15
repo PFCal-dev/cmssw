3364ba5632cbd22d27ef334619cd389bd9be313c
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2019-04-15 10:01:49 +0200

    Apply code-format L1THGCal

--- L1Trigger/L1THGCal/interface/veryfrontend/HGCalVFESummationImpl.h
+++ L1Trigger/L1THGCal/interface/veryfrontend/HGCalVFESummationImpl.h
@@ -11,19 +11,17 @@
 #include <array>
 #include <vector>
 
-class HGCalVFESummationImpl
-{
-
- public:
+class HGCalVFESummationImpl {
+public:
   HGCalVFESummationImpl(const edm::ParameterSet& conf);
-  
-  void eventSetup(const edm::EventSetup& es) {triggerTools_.eventSetup(es);}
-  void triggerCellSums(const HGCalTriggerGeometryBase& ,
-                       const std::vector<std::pair<DetId, uint32_t > >&,
+
+  void eventSetup(const edm::EventSetup& es) { triggerTools_.eventSetup(es); }
+  void triggerCellSums(const HGCalTriggerGeometryBase&,
+                       const std::vector<std::pair<DetId, uint32_t> >&,
                        std::unordered_map<uint32_t, uint32_t>& payload);
    
  private:
-  std::vector<double> thickness_corrections_;   
+  std::vector<double> thickness_corrections_;
   double LSB_silicon_fC_;
   double LSB_scintillator_MIP_;
   std::vector<double> thresholds_silicon_;
