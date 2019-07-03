c7e62c44a07e3199ad205fe4012efc305e66e3bf
federico de guio <federico.de.guio@cern.ch>
2019-07-03 17:02:31 +0200

    Automatically define 1D-block grid

--- SimCalorimetry/HGCalSimProducers/src/HGCDigitizerBase.cu
+++ SimCalorimetry/HGCalSimProducers/src/HGCDigitizerBase.cu
@@ -7,7 +7,8 @@
 __global__
 void addNoise(int n, float* cellCharge, float* cellToa, bool weightMode, float* devRand, uint8_t* cellType, uint* word)
 {
-  int i = blockIdx.x*blockDim.x + threadIdx.x;
+  int i = threadIdx.x + blockDim.x*blockIdx.x + blockDim.x*gridDim.x*blockDim.y*blockIdx.y;
+  printf("\ni = %d", i);
   if (i >= n)
     return;
 
@@ -25,8 +26,6 @@ void addNoise(int n, float* cellCharge, float* cellToa, bool weightMode, float* 
   totalCharge += std::max(randNum*noise[type], 0.f);
   if(totalCharge<0.f) totalCharge=0.f;
 
-
-
   bool passThr=(totalCharge>0.672);
   uint16_t finalCharge=(uint16_t)(fminf( totalCharge, 100.)/0.0977);
   uint16_t finalToA=(uint16_t)(toa/0.0244);
@@ -40,11 +39,38 @@ void addNoiseWrapper(int n, float* cellCharge, float* cellToa, bool weightMode, 
 {    
 
   //Generate n floats on device
-  std::cout << "--> N " << n << std::endl;
+  std::cout << "\n--> n = " << n << std::endl;
   curandGenerateNormal(gen, devRand, n, 0.f, 1.f);
-  std::cout << "--> DONE" << std::endl;
+  std::cout << "--> DONE generation" << std::endl;
 
   //call function on the GPU
-  addNoise<<<(n+255)/256, 256>>>(n, cellCharge, cellToa, weightMode, devRand, cellType, word);
+  int device_id = 0;
+  int maxThreadsPerBlock = -1;
+  cudaDeviceGetAttribute(&maxThreadsPerBlock, cudaDevAttrMaxThreadsPerBlock, device_id);
+
+  int minBlocksNum = n / maxThreadsPerBlock  +  n % maxThreadsPerBlock;
+  int blocksDimXNum = n / maxThreadsPerBlock  +  n % maxThreadsPerBlock;
+  int maxBlockDimX = -1;
+  cudaDeviceGetAttribute(&maxBlockDimX, cudaDevAttrMaxBlockDimX, device_id);
+
+  int blocksDimYNum = 1;
+  int maxBlockDimY = -1;
+  cudaDeviceGetAttribute(&maxBlockDimY, cudaDevAttrMaxBlockDimY, device_id);
+  if (minBlocksNum > maxBlockDimX) {
+    std::cout <<"minBlocksNum (" <<minBlocksNum <<") for a 1D-block 1D kernel is larger than maxBlockDimX (" <<maxBlockDimX <<"), will consider a 1D-block 2D kernel!" <<std::endl;
+    blocksDimXNum = maxBlockDimX;
+    int minBlocksYNum = minBlocksNum / maxBlockDimX + minBlocksNum % maxBlockDimX;
+
+    if (minBlocksYNum > maxBlockDimY) {
+      std::cout <<"minBlocksYNum (" <<minBlocksYNum <<") for a 1D-block 2D kernel is larger than maxBlockDimY (" <<maxBlockDimY <<"), will run a 1D-block 3D kernel!" <<std::endl;
+      return;
+    }
+
+    blocksDimYNum = minBlocksYNum;
+  }
+
+  dim3 dimGrid(blocksDimXNum, blocksDimYNum);
+  std::cout <<"Running kernel with " <<blocksDimXNum <<" x-blocks, " <<blocksDimYNum <<" y-blocks and " <<maxThreadsPerBlock <<" threads per block" <<std::endl;
+  addNoise<<<dimGrid, maxThreadsPerBlock>>>(n, cellCharge, cellToa, weightMode, devRand, cellType, word);
   std::cout << "--> DONE NOISE" << std::endl;
 }         
